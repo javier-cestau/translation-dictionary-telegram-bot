f675f09d4bcd858e36c1176e203f83b03996a733
javier <javier.cestau1209@gmail.com>
2019-06-12 12:40:39 +0200

    adding dalli

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-
+  config.cache_store = :dalli_store
+  
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
@@ -90,6 +91,6 @@ Rails.application.configure do
     # host: 'translation-telegram-bot.herokuapp.com',
     protocol: 'https'
   }
-  
+
   
 end
