557e5549361d2e8771b4cce2b036f3a2544c3232
javier <javier.cestau1209@gmail.com>
2019-06-12 14:05:10 +0200

    memory_store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.telegram_updates_controller.session_store = :dalli_store
+  config.telegram_updates_controller.session_store = :memory_store
   
   routes.default_url_options = {
     host: 'dictionary-telegram-bot.herokuapp.com',
