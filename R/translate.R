34a242b034d7e0617808eb55661889657da788a8
javier <javier.cestau1209@gmail.com>
2019-06-13 22:09:25 +0200

    curl on script

--- R/translate.R
+++ R/translate.R
@@ -1,9 +1,10 @@
 library(dplyr)
 library(rvest)
 library(stringi)
+library(curl)
 
 translate <- function(url){
-  site <- read_html(iconv(url, to = "UTF-8"), encoding = "utf8")
+  site <- read_html(curl(iconv(url, to = "UTF-8"), handle = curl::new_handle("useragent" = "Mozilla/5.0")), encoding = "utf8")
   
   from_word <- site %>%
     html_nodes('#articleWRD table.WRD:first-of-type .even .FrWrd strong') %>%
