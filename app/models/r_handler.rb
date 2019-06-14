cd288ee8e42cb7709f0b818ca1dc796a4489889f
javier <javier.cestau1209@gmail.com>
2019-06-14 20:40:51 +0200

    changing to google.com

--- app/models/r_handler.rb
+++ app/models/r_handler.rb
@@ -4,7 +4,7 @@ class RHandler
                                  : ::Rserve::Simpler.new
     end
     def source(file, word, language_source, language_translation)
-        url = "https://www.wordreference.com/#{language_source}#{language_translation}/#{word}"
+        url = "https://www.google.com/"
 
         conn = Faraday.new do |connection|
             connection.use FaradayMiddleware::FollowRedirects
