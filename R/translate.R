4c2d127266e10d1c0845cab0a6a064fbf6eb26ac
javier <javier.cestau1209@gmail.com>
2019-06-14 11:33:09 +0200

    other way to make request

--- R/translate.R
+++ R/translate.R
@@ -1,24 +1,20 @@
 library(dplyr)
 library(rvest)
 library(stringi)
-library(curl)
 
-translate <- function(url){
-  download.file(url, destfile = "scrapedpage.html", quiet=TRUE)
-
-  site <- read_html("scrapedpage.html", encoding = "utf8")
+translate <- function(url_request){
+  conn <- url(url_request, 'rb')
+  site <- read_html(conn, encoding = "utf8")
   
   from_word <- site %>%
     html_nodes('#articleWRD table.WRD:first-of-type .even .FrWrd strong') %>%
     html_text()
-    
+  
   to_word <- site %>% 
     html_nodes('#articleWRD table.WRD:first-of-type .even .ToWrd') %>%
     html_nodes(xpath = 'text()') %>%
     html_text()
-
   
   assign("to_word", URLencode(to_word),  envir = .GlobalEnv)
   assign("from_word", URLencode(from_word),  envir = .GlobalEnv)
-  
 }
