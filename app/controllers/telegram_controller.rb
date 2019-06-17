c121f68c6166a4b191442ebdefe57ba132505f45
javier <javier.cestau1209@gmail.com>
2019-06-17 15:39:39 +0200

    order code on telegram controller

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -41,10 +41,9 @@ class TelegramController < Telegram::Bot::UpdatesController
         ) 
 
         from_word_unformatted =  r.get_var('from_word')
+        to_word_unformatted =  r.get_var('to_word')
         
         from_word_formatted = from_word_unformatted.class == Array ?  from_word_unformatted.uniq.map(&:strip).join(', ') : from_word_unformatted
-
-        to_word_unformatted =  r.get_var('to_word')
         to_word_formatted = to_word_unformatted.class == Array ?  to_word_unformatted.uniq.map(&:strip).join(', ') : to_word_unformatted
 
         language_from = WORDREFERENCE_LANGUAGES[@chat_config.language_source.to_sym][:icon]
