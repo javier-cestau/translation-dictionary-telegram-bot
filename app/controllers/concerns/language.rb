4aad0d649a12a74a9f196444e5b52b1bc3be02ea
javier <javier.cestau1209@gmail.com>
2019-06-21 18:46:48 +0200

    adding command translation

--- app/controllers/concerns/language.rb
+++ app/controllers/concerns/language.rb
@@ -6,9 +6,8 @@ module Language
         WORDREFERENCE_LANGUAGES.each do |code, language_info|
             options.push("#{language_info[:icon]} #{t("languages.#{code}")}: #{code}")
         end
-        text = t('command.language_source.text', { options: options.join("\n")})
+        text = t('command.language.initial_text', { options: options.join("\n")})
 
-        
         save_context :language_source
         respond_with :message, text: text, parse_mode: 'Markdown'
     end
@@ -18,7 +17,7 @@ module Language
                                                            : language_source.downcase
 
         if language_source_code.nil?
-            return respond_with :message, text: t('command.language_source.error'), parse_mode: 'Markdown'
+            return respond_with :message, text: t('command.language.error'), parse_mode: 'Markdown'
         end       
                                                     
         options = []
@@ -28,7 +27,7 @@ module Language
 
         language_source_text = "#{WORDREFERENCE_LANGUAGES[language_source_code.to_sym][:icon]} #{t("languages.#{language_source_code}")}"
         
-        text = t('command.language_translation.text', { options: options.join("\n"), language_source: language_source_text }) 
+        text = t('command.language.second_text', { options: options.join("\n"), language_source: language_source_text }) 
      
         session[:language_source] = language_source_code
         save_context :language_translation
@@ -40,14 +39,19 @@ module Language
         translation_code = translation.length != 2 ? convert_word_language_to_code(translation.downcase) 
                                                    : translation.downcase
         if translation_code.nil?
-            return respond_with :message, text: t('command.language_source.error'), parse_mode: 'Markdown'
+            return respond_with :message, text: t('command.language.error'), parse_mode: 'Markdown'
         end       
                                         
         @chat_config.update(language_translation: translation_code, language_source: session[:language_source] )
 
         language_from = "#{t("languages.#{@chat_config.language_source}")} #{WORDREFERENCE_LANGUAGES[@chat_config.language_source.to_sym][:icon]}"
         language_to = "#{t("languages.#{@chat_config.language_translation}")} #{WORDREFERENCE_LANGUAGES[@chat_config.language_translation.to_sym][:icon]}"
-        respond_with :message, text: "We'll translate from *#{language_from}* to *#{language_to}*", parse_mode: 'Markdown'
+        respond_with :message, 
+                    text: I18n.t('command.language.translate_from_to',  {
+                                                                            language_from: language_from, 
+                                                                            language_to: language_to 
+                                                                        }), 
+                    parse_mode: 'Markdown'
     end
     
     private 
